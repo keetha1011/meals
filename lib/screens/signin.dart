f56b7c87eebd738f0a66a0cfb537daa426653d76
Keerthan <136081248+keetha1011@users.noreply.github.com>
2024-01-31 21:58:19 +0530

    login and logout functions

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:project02_hackloop/screens/home.dart';
 import 'package:project02_hackloop/utils/color.dart';
 import 'package:project02_hackloop/widgets/reusable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -13,38 +14,64 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   TextEditingController _passwordTextController = TextEditingController();
   TextEditingController _emailTextController = TextEditingController();
+
+  void addAuthentication() {
+    try {
+      final credential = FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailTextController.text,
+          password: _passwordTextController.text);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      } else {
+        print("Auth done!");
+      }
+    }
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        // !!!!! Here you know the user is signed-in !!!!!
+        print('User is signed in!');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
             colors: [toColor("BB1009"), toColor("610000")],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           )),
           child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(20, MediaQuery.of(context).size.height*0.1 , 20, 0),
-              child: Column(
-                children: <Widget>[
-                  logoWidget("assets/logo/meals.png",320,240),
-                  reusableTextField("Enter UserName", Icons.person_outline, false,_emailTextController),
-                  const SizedBox(height: 20,),
-                  reusableTextField("Enter Password", Icons.lock_outline, true,_passwordTextController),
-                  const SizedBox(height: 5),
-                  uiButton(context, "Login", () {
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()));
-                  }),
-                  ImageWidget("assets/images/banner.png", 300, 240),
-                ]
+              child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.1, 20, 0),
+            child: Column(children: <Widget>[
+              logoWidget("assets/logo/meals.png", 320, 240),
+              reusableTextField("Enter UserName", Icons.person_outline, false,
+                  _emailTextController),
+              const SizedBox(
+                height: 20,
               ),
-            )
-        )
-      ),
+              reusableTextField("Enter Password", Icons.lock_outline, true,
+                  _passwordTextController),
+              const SizedBox(height: 5),
+              uiButton(context, "Login", () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()));
+              }),
+              ImageWidget("assets/images/banner.png", 300, 240),
+            ]),
+          ))),
     );
   }
 }
